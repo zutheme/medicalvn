123c953799ea91a8d9ee57de368abd1fb8626d30
hatazu <hatazu@gmail.com>
2020-11-08 23:07:35 +0700

    db

--- wp-content/themes/htz_thienkhue/layout/connect-facebook.php
+++
@@ -1,9 +0,0 @@
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-<div class="right_blog_category_wrapper">
-    <h4 class="med_bottompadder10">Kết nối</h4>
-        
-            <div class="fb-page" data-href="https://www.facebook.com/TickMedical/" data-width="390" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/TickMedical/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/thammyvienthienkhue/">
-Phòng khám xương khớp Tick Medical</a></blockquote></div>
-        </div>
-    </div>
-</div>
