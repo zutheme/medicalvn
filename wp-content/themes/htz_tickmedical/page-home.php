c11a71d3793337d9602b28448674a7694fe3e576
hatazu <hatazu@gmail.com>
2020-11-23 09:44:34 +0700

    sd

--- wp-content/themes/htz_tickmedical/page-home.php
+++ wp-content/themes/htz_tickmedical/page-home.php
@@ -17,6 +17,7 @@ get_header(); ?>
 get_template_part('layout/slider-tick');
 get_template_part('layout/work-time');
 get_template_part('layout/search-block');
+get_template_part('layout/register-form-gift');
 get_template_part('layout/about');
 get_template_part('layout/our-services');
 //get_template_part('layout/portfolio-video');
@@ -36,7 +37,7 @@ get_template_part('layout/blog-news');
 //get_template_part('layout/blog-hoidap');
 //get_template_part('layout/portfolio-menu');
 get_template_part('layout/register-form');
-get_template_part('layout/register-form-gift');
+
 //get_template_part('layout/partner'); ?>
 
 <?php get_footer(); ?>
