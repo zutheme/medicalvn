353f614cdb4d11d6647e0eeceafd5cb762c6bebc
hatazu <hatazu@gmail.com>
2020-11-25 15:58:56 +0700

    content page

--- wp-content/themes/htz_tickmedical/page.php
+++ wp-content/themes/htz_tickmedical/page.php
@@ -42,7 +42,8 @@ get_header();
 					<?php
 					while ( have_posts() ) :
 						the_post();
-						get_template_part( 'template-parts/content', 'page' );
+						//get_template_part( 'template-parts/content', 'page' );
+                        the_content(); 
 						// If comments are open or we have at least one comment, load up the comment template.
 						//if ( comments_open() || get_comments_number() ) :
 							//comments_template();
